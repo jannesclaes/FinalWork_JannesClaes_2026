b99aace3b126b4459c99d9dcf003126db9a2a595
jannesclaes <jannes.claes@student.ehb.be>
2026-04-28 11:37:57 +0200

    Feat: Data wordt van api gehaald en doorgestuurd naar touchdesigner via OSC

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import fastf1
+from pythonosc import udp_client  
+
+client = udp_client.SimpleUDPClient("127.0.0.1", 7000)
 
 session = fastf1.get_session(2026, 1, 'R')
-
-session.load()
-
+session.load(laps=True)
 lap_12 = session.laps.pick_laps(12).sort_values(by='Position')
 
-print(lap_12[['Position', 'DriverNumber']])
+for index, row in lap_12.iterrows():
+    address = f"/driver/pos{int(row['Position'])}"
+    print(address, int(row['DriverNumber']))
+    client.send_message(address, int(row['DriverNumber']))
+
+print("Data succesvol verzonden")
